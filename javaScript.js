acf24ac3fd4926c6022af26d01edd7cd81c85cfd
Pascal-Rutter <64703956+Pascal-Rutter@users.noreply.github.com>
2022-09-13 12:04:11 +0200

    Add hoverFeature

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,14 @@
+function changeToHovered(){
+    this.setAttribute("class", "hovered")
+}
+
 for (i = 0; i < 16; i++) {
     for(j = 0; j < 16; j++){
         let square = document.createElement("div");
         square.setAttribute("id", `square[${i},${j}]`);
         square.setAttribute("class", "square");
-
+        
+        square.addEventListener('mouseover', changeToHovered);
         document.querySelector("div").appendChild(square);
     }
 }
